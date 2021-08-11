753e9cc3f32c62ef5966206811c6755e8aca7425
soosbk <soobin851@kookmin.ac.kr>
2021-08-11 21:16:58 +0900

    글 작성 까지는 마무리

--- party_party/goods/urls.py
+++ party_party/goods/urls.py
@@ -7,12 +7,13 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('goodslist/', goodslist, name = "goodslist"),
-    path('goodsdetail/<str:product_id>',Semigoodsdetail, name="Semigoodsdetail"),
+    path('goodsdetail/<str:product_id>',goodsdetail, name="Semigoodsdetail"),
     path('goodsdetail/<str:product_id>', goodsdetail, name="goodsdetail"),
     path('write_semigoods/', write_semigoods, name="write_semigoods"),
     path('create_semi/', create_semi,name='create_semi'),
     path('write_goods/', write_goods, name = "write_goods"),
     path('create/', create,name='create'),
+
     path('write_choices/',write_choices,name="write_choices"),
     path('mypage/', mypage, name='mypage'),
     path('myfavp/',myfavp,name='myfavp'),
